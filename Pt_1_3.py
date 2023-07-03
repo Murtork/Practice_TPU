fe432402781cea878fe3e51e6d139488a9bf0787
Murtork <egor20042004@mail.ru>
2023-07-03 22:52:37 +0700

    Pt_1_3: Full Task
    This is my full task.

---
+++ Pt_1_3.py
@@ -0,0 +1,25 @@
+a=int(input())
+b=int(input())
+c=int(input())
+
+if ((a>b) & (a>c)):
+    if b>c:
+        print("a - самое большое число")
+        print(a,b,c)
+    else:
+        print("a - самое большое число")
+        print(a,c,b)
+if ((c>b) & (c>a)):
+    if b>a:
+        print("c - самое большое число")
+        print(c,b,a)
+    else:
+        print("c - самое большое число")
+        print(c,a,b)
+if ((b>c) & (b>a)):
+    if c>a:
+        print("a - самое большое число")
+        print(b,c,a)
+    else:
+        print("a - самое большое число")
+        print(b,a,c)
