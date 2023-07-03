39917c35ae865fcf10dd5c84c1ea2bea51e65582
Murtork <egor20042004@mail.ru>
2023-07-03 23:39:53 +0700

    Pt_2_1: Full task
    This is full task.

---
+++ Pt_2_1.py
@@ -0,0 +1,37 @@
+def findout(answer):
+    print("Попробуй угадать цвет который я загадал: ")
+    ans=str(input())
+    if ans==answer:
+        print("Отлично!")
+    else:
+        print("Подсказка:")
+        if answer=="Зеленый":
+            print("Цвет начинается на букву З")
+            findout(answer)
+        if answer=="Желтый":
+            print("Цвет начинается на букву Ж")
+            findout(answer)
+        if answer=="Красный":
+            print("Цвет начинается на букву К")
+            findout(answer)
+        if answer=="Синий":
+            print("Цвет начинается на букву С")
+            findout(answer)
+        if answer=="Розовый":
+            print("Цвет начинается на букву Р")
+            findout(answer)
+
+import random
+a=random.randint(1,5)
+print(a)
+if a==1:
+    answer="Зеленый"
+if a==2:
+    answer="Желтый"
+if a==3:
+    answer="Красный"
+if a==4:
+    answer="Синий"
+if a==5:
+    answer="Розовый"
+findout(answer)
