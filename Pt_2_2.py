b6481b58ca7ecaa510f27e68976d0ab1afea2b58
Murtork <egor20042004@mail.ru>
2023-07-07 16:10:24 +0700

    Pt_2_2: Full task
    This is full task

---
+++ Pt_2_2.py
@@ -0,0 +1,27 @@
+a=[0]*5
+a[0]="Зимородок"
+a[1]="Хороший доктор"
+a[2]="Ведьмак"
+a[3]="Уэнсдей"
+
+b=[0]*5
+b[0]="Зимородок"
+b[1]="Хороший доктор"
+b[2]="Ведьмак"
+b[3]="Уэнсдей"
+
+for i in range(5): 
+    print(a[i])
+print()
+
+nomer=int(input("Введите номер вашей передачи: "))
+nomer=nomer-1
+nazv=str(input("Название передачи: "))
+
+for i in range(4):
+    if i>=nomer:
+        a[i+1]=b[i]
+a[nomer]=nazv
+print()
+for i in range(5):
+    print(a[i])
