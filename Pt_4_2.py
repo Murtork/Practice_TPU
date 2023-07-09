f12f62752ee1b3c7d5c03c0abeeb121b1cd9e0d9
Murtork <egor20042004@mail.ru>
2023-07-09 17:34:23 +0700

    Pt_4_2: Palindromes 0_o
    This is full task

---
+++ Pt_4_2.py
@@ -0,0 +1,13 @@
+def find_p(mes):
+    answer = []
+    for i in range(len(mes)):
+        for j in range(i + 1, len(mes) + 1):
+            subst = mes[i:j]
+            if subst == subst[::-1] and len(subst) > 1:
+                answer.append(subst)
+    return answer
+
+st = input("Строка: ")
+palind = find_p(st)
+print("Палиндромы:", palind)
+
