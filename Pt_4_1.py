e1371f265753fc8c732e6d038dcdeb1a9b20635b
Murtork <egor20042004@mail.ru>
2023-07-09 17:34:07 +0700

    Pt_4_1: Biggest Change
    This is full task

---
+++ Pt_4_1.py
@@ -0,0 +1,15 @@
+a=int(input())
+b=[]
+for i in str(a):
+    b.append(i)
+#print(b)
+for i in range(len(b)-1):
+    for j in range(len(b)-i-1):
+        if int(b[j])<int(b[j+1]):
+            b[j], b[j+1] = b[j+1], b[j]
+#print(b)
+c=""
+for i in range(len(b)):
+    c=c+str(b[i])
+print(c)
+
