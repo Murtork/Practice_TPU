eda4773ab481d2233983a438f3a0310c42b6ca26
Murtork <egor20042004@mail.ru>
2023-07-09 17:31:01 +0700

    Pt_3_7: Vowels
    This is full task.

---
+++ Pt_3_7.py
@@ -0,0 +1,14 @@
+d = {
+    "й": False, "х": False, "д": False, "б": False, 
+    "ц": False, "ъ": False, "ж": False, "ю": True, 
+    "у": True, "ф": False, "э": True,
+    "к": False, "ы": True, "я": True, 
+    "е": True, "в": False, "ч": False, 
+    "н": False, "а": True, "с": False, 
+    "г": False, "п": False, "м": False, 
+    "ш": False, "р": False, "и": True, 
+    "щ": False, "о": True, "т": False, 
+    "з": True, "л": False, "ь": False,
+    }
+x=d['я']
+print(x)
