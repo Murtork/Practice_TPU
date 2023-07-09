72672c9cfe417a37348024894251dee4d89ea5e8
Murtork <egor20042004@mail.ru>
2023-07-09 17:36:16 +0700

    Pt_4_7: Finding the Changes
    This is full task

---
+++ Pt_4_7.py
@@ -0,0 +1,5 @@
+from itertools import permutations
+lst = input('Строчка для поиска перестановок: ').split(',')
+permutations = permutations(lst)
+for permutations in permutations:
+    print(permutations)
