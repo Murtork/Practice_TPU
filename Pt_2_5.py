05f637b72df889c5440349d6e27f2b80664febf4
Murtork <egor20042004@mail.ru>
2023-07-18 17:38:28 +0700

    Pt_2_5: Coin Game
    Updated

--- Pt_2_5.py
+++ Pt_2_5.py
@@ -1,6 +1,7 @@
 import random
+
 win = defeat = 0
-health=3
+health = 3
 while True:
     print()
     user = input("Орел - 0 или Решка - 1 \n")
@@ -8,13 +9,13 @@ while True:
     if user not in ("01"):
         print("Конец игры")
         break
-    pc=random.choice("01")
-    if pc==user:
-        win+=1
+    pc = random.choice("01")
+    if pc == user:
+        win += 1
     else:
-        defeat+=1
-        health-=1
-    print(f'Побед: {win}\nПоражений: {defeat}\nБаланс: {balance}')
-    if balance <=0:
+        defeat += 1
+        health -= 1
+    print(f'Побед: {win}\nПоражений: {defeat}\nБаланс: {health}')
+    if health <= 0:
         print("Кончились жизни")
         break
