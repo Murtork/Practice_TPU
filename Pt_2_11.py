cd0f9fe1e63217effb70410988d3c934d5237a20
Murtork <egor20042004@mail.ru>
2023-07-18 17:39:05 +0700

    Pt_2_11: Sum from A until B
    Updated

--- Pt_2_11.py
+++ Pt_2_11.py
@@ -1,6 +1,6 @@
-a=int(input())
-b=int(input())
-summ=0
-for i in range(a,b+1):
-    summ=summ+i
+a = int(input())
+b = int(input())
+summ = 0
+for i in range(a, b + 1):
+    summ = summ + i
 print(summ)
