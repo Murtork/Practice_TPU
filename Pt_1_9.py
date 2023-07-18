44e546137b7445c063744ecf9dc89cd9a90c1ca2
Murtork <egor20042004@mail.ru>
2023-07-18 17:16:08 +0700

    Pt_1_9: Multiplication
    Updated

--- Pt_1_9.py
+++ Pt_1_9.py
@@ -1,3 +1,3 @@
-a=int(input())
-b=int(input())
-print("Произведение чисел ",a ," и ",b, " равно ", a*b,sep='')
+a = int(input())
+b = int(input())
+print("Произведение чисел ", a, " и ", b, " равно ", a * b, sep='')
