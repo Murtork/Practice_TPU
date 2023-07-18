13793d9d3c9380a03aa480695e5e3828bf786770
Murtork <egor20042004@mail.ru>
2023-07-18 18:04:27 +0700

    Pt_4_4: Wonderful Combinations
    Updated

--- Pt_4_4.py
+++ Pt_4_4.py
@@ -1,9 +1,9 @@
 from itertools import combinations
 
-numbs=[1, 3, 4, 7, 8, 2]
-a=int(input("Введите число: "))
-uniq_com=[]
-sum_com=[]
+numbs = [1, 3, 4, 7, 8, 2]
+a = int(input("Введите число: "))
+uniq_com = []
+sum_com = []
 
 for r in range(1, len(numbs) + 1):
     combs = list(map(list, combinations(numbs, r)))
@@ -14,4 +14,3 @@ for combination in uniq_com:
         sum_com.append(combination)
 
 print(f'Подходящие коомбинации: {sum_com}')
-
