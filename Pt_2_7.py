6745424b017463055506ed38c3f70922e98f56a9
Murtork <egor20042004@mail.ru>
2023-07-18 17:38:39 +0700

    Pt_2_7: Dont enter <0
    Updated

--- Pt_2_7.py
+++ Pt_2_7.py
@@ -1,10 +1,9 @@
-sum=0
+sum = 0
 while True:
-    a=int(input())
-    if a>0:
-        sum+=a
+    a = int(input())
+    if a > 0:
+        sum += a
         print(sum)
     else:
         print("это конец.")
         break
-        
