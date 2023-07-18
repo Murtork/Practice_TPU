59acb7cb553001751bc48293a18531b9bc0b0f15
Murtork <egor20042004@mail.ru>
2023-07-18 17:38:45 +0700

    Pt_2_8: First Square
    Updated

--- Pt_2_8.py
+++ Pt_2_8.py
@@ -1,5 +1,5 @@
-a=int(input())
+a = int(input())
 for i in range(100000):
-    if i*i>a:
-        print("Квадрат этого числа больше заданного:",i)
+    if i * i > a:
+        print("Квадрат этого числа больше заданного:", i)
         break
