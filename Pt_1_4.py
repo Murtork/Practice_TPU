ce994005efdcd6e3ff72386a89041f9bf054fbc1
Murtork <egor20042004@mail.ru>
2023-07-18 17:15:25 +0700

    Pt_1_4: 10 - 20
    Updated

--- Pt_1_4.py
+++ Pt_1_4.py
@@ -1,12 +1,15 @@
 def da():
-    a=int(input("Введите число"))
-    if (a >= 10):
-        if (a <= 20):
+    a = int(input("Введите число от 10 до 20: "))
+    if a >= 10:
+        if a <= 20:
             print("Спасибо")
             return 0
         else:
             print("Число больше требуемого")
+            return da()
     else:
         print("Число меньше требуемого")
         return da()
+
+
 da()
