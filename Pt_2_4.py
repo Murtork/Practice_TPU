04d55253eb3d7f171a8738f69c0c62386badcf4b
Murtork <egor20042004@mail.ru>
2023-07-18 17:38:20 +0700

    Pt_2_4: Roots
    Updated

--- Pt_2_4.py
+++ Pt_2_4.py
@@ -1,14 +1,14 @@
-a=int(input())
-b=int(input())
-c=int(input())
-d=b**2-4*a*c
+a = int(input())
+b = int(input())
+c = int(input())
+d = b ** 2 - 4 * a * c
 print(d)
-if d<0:
+if d < 0:
     print("Корней нет")
-elif d==0:
-    x1=(-b)/2*a
-    print("Корень равен:",x1)
+elif d == 0:
+    x1 = (-b) / 2 * a
+    print("Корень равен:", x1)
 else:
-    x1=(-b+d**(1/2))/(2*a)
-    x2=(-b-d**(1/2))/(2*a)
-    print("Корни равны:", x1,x2)
+    x1 = (-b + d ** (1 / 2)) / (2 * a)
+    x2 = (-b - d ** (1 / 2)) / (2 * a)
+    print("Корни равны:", x1, x2)
